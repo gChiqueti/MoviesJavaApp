261ab3945bcb35b7b2ade3fdee479c51924ebbd3
gChiqueti <36969020+gChiqueti@users.noreply.github.com>
2020-06-21 17:12:39 -0300

    alterado visualiza

--- app/src/main/java/com/example/testetokenlab/MainActivity.java
+++ app/src/main/java/com/example/testetokenlab/MainActivity.java
@@ -5,13 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.LinkAddress;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -26,12 +30,18 @@ import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import pl.droidsonroids.gif.GifImageView;
 
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_TEXT = "com.example.testetokenlab.example.EXTRA_TEXT";
     public static final String ENDPOINT_URL = "https://desafio-mobile.nyc3.digitaloceanspaces.com/movies";
 
+    int idMainLoadingGif;
+    LinearLayout ll;
+    LinearLayout.LayoutParams lp;
+
+    OkHttpClient client;
 
 
     @Override
@@ -41,7 +51,18 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        OkHttpClient client;
+        ll = (LinearLayout) findViewById(R.id.layout);
+        lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        GifImageView imgMainLoadingGif;
+        imgMainLoadingGif = new GifImageView(this);
+        imgMainLoadingGif.setImageResource(R.drawable.loading);
+        idMainLoadingGif = View.generateViewId();
+        imgMainLoadingGif.setId(idMainLoadingGif);
+        ll.addView(imgMainLoadingGif, lp);
+
         client = new OkHttpClient();
         Request request = new Request.Builder().url(ENDPOINT_URL).build();
 
@@ -70,8 +91,69 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    LinearLayout createNewMovieObject(Bitmap poster, String id, String title, String rating)
+    {
+        // Estilo
+        // - Horizontal Layout
+        //    - Imagem do poster
+        //    - Vertical layout
+        //       - Titulo
+        //       - Rating
+
+        LinearLayout parent = new LinearLayout(this);
+        parent.setOrientation(LinearLayout.HORIZONTAL);
+        parent.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                                                             LinearLayout.LayoutParams.WRAP_CONTENT));
+
+        ImageView imgPoster = new ImageView(this);
+        imgPoster.setPadding(10, 5, 10, 5);
+
+        if (poster != null) {
+            imgPoster.setImageBitmap(poster);
+        } else {
+            imgPoster.setImageResource(R.drawable.no_image);
+        }
+        float scale = getResources().getDisplayMetrics().density;
+        imgPoster.setLayoutParams(new LinearLayout.LayoutParams((int) (100*scale), (int) (150*scale)));
+        imgPoster.requestLayout();
+        imgPoster.setClickable(true);
+        imgPoster.setTag(id);
+        imgPoster.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String id = (String) v.getTag();
+                changeActivity(id);
+            }
+        });
+        parent.addView(imgPoster);
+
+        LinearLayout layout2 = new LinearLayout(this);
+        layout2.setOrientation(LinearLayout.VERTICAL);
+        layout2.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                                                              LinearLayout.LayoutParams.WRAP_CONTENT));
+        parent.addView(layout2);
+
+//children of layout2 LinearLayout
+
+        TextView tv1 = new TextView(this);
+        tv1.setText(title);
+        tv1.setTextSize((int)scale*15);
+        tv1.setTextColor(Color.parseColor("#000000"));
+        tv1.setPadding(5, 10, 10, 0);
+        layout2.addView(tv1);
+
+        TextView tv2 = new TextView(this);
+        tv2.setText(rating);
+        tv2.setPadding(5, 5, 10, 0);
+        layout2.addView(tv2);
+
+        return parent;
+    }
+
     void retrieveDataFromString(String jsonString)
     {
+
+
         JSONArray jsonArray = null;
         try {
             jsonArray = new JSONArray(jsonString);
@@ -80,40 +162,22 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-        LinearLayout ll = (LinearLayout) findViewById(R.id.layout);
-        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-
         for (int i = 0; i < jsonArray.length(); i++) {
             try {
                 String title = jsonArray.getJSONObject(i).getString("title");
-                Button myButton = new Button(this);
-                myButton.setText(title);
-                ll.addView(myButton, lp);
-
-            }catch (JSONException e) {}
-
-            try {
+                String rating = jsonArray.getJSONObject(i).getString("vote_average");
                 String url = jsonArray.getJSONObject(i).getString("poster_url");
                 String id = jsonArray.getJSONObject(i).getString("id");
-                Log.i("URL", url);
                 Bitmap bitmap = new GetBitmapFromURL().execute(url).get();
-                if (bitmap != null) {
-                    ImageView v = new ImageView(this);
-                    v.setImageBitmap(bitmap);
-                    v.setTag(id);
-                    v.setClickable(true);
-                    v.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            String id = (String) view.getTag();
-                            changeActivity(id);
-                        }
-                    });
-                    ll.addView(v, lp);
-                }
+                LinearLayout l = createNewMovieObject(bitmap, id, title, rating);
+                ll.addView(l, lp);
             } catch (Exception e) {}
 
-        }  // endfor
+        }
+
+        View loadingView = this.findViewById(idMainLoadingGif);
+        ViewGroup vg = ((ViewGroup) loadingView.getParent());
+        vg.removeView(loadingView);
 
     }
 
