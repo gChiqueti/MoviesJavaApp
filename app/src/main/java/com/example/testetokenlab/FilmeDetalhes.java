b76dbb25e0844c8afcfb4ce530f2414789baee69
gChiqueti <36969020+gChiqueti@users.noreply.github.com>
2020-06-22 09:03:27 -0300

    adicionado captura da imagem backdrop e o texto dos produtores e inserido aba detalhes

--- app/src/main/java/com/example/testetokenlab/FilmeDetalhes.java
+++ app/src/main/java/com/example/testetokenlab/FilmeDetalhes.java
@@ -4,16 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.concurrent.ExecutionException;
 
 import okhttp3.Call;
@@ -29,7 +33,10 @@ public class FilmeDetalhes extends AppCompatActivity {
     TextView title;
     TextView tagLine;
     ImageView poster;
+    ImageView backdrop;
+    TextView productedBy;
 
+    OkHttpClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,14 +48,14 @@ public class FilmeDetalhes extends AppCompatActivity {
         genre = (TextView) findViewById(R.id.textGenre);
         tagLine = (TextView) findViewById(R.id.textTagline);
         poster = (ImageView) findViewById(R.id.imgPoster);
+        backdrop = (ImageView) findViewById(R.id.imgBackdrop);
+        productedBy = (TextView) findViewById(R.id.textProductedBy);
 
         Intent intent = getIntent();
         String url = intent.getStringExtra(MainActivity.EXTRA_TEXT);
 
-        OkHttpClient client;
         client = new OkHttpClient();
         Request request = new Request.Builder().url(url).build();
-
         client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
@@ -76,6 +83,7 @@ public class FilmeDetalhes extends AppCompatActivity {
 
     public void retrieveDataFromString(String jsonString)
     {
+
         JSONObject jsonObject;
         try {
             jsonObject = new JSONObject(jsonString);
@@ -91,6 +99,41 @@ public class FilmeDetalhes extends AppCompatActivity {
             e.printStackTrace();
         }
 
+
+        String urlBackdrop = null;
+        try {
+            urlBackdrop = jsonObject.getString("backdrop_url");
+            Request request = new Request.Builder().url(urlBackdrop).build();
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    e.printStackTrace();
+                    Log.i("ERRO:", "Captura de string da URL falhou");
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    final Bitmap bmp;
+                    if (response.isSuccessful()) {
+                        Log.i("SUCESSO:", "Captura de string da URL foi bem sucedida");
+                        InputStream is = response.body().byteStream();
+                        bmp = BitmapFactory.decodeStream(is);
+                    } else {
+                        bmp = null;
+                    }
+                    FilmeDetalhes.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            backdrop.setImageBitmap(bmp);
+                        }
+                    });
+                }
+            });
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
         String strOverview = null;
         try {
             strOverview = jsonObject.getString("overview");
@@ -101,6 +144,20 @@ public class FilmeDetalhes extends AppCompatActivity {
         String strTagLine = null;
         try {
             strTagLine = jsonObject.getString("tagline");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JSONArray strProductedBy = null;
+        String producers = "";
+        try {
+            strProductedBy = jsonObject.getJSONArray("production_companies");
+            for (int i = 0; i < strProductedBy.length(); i++)
+            {
+                String companyName = strProductedBy.getJSONObject(i).getString("name");
+                producers = producers.concat(companyName + ", ");
+            }
+
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -120,9 +177,22 @@ public class FilmeDetalhes extends AppCompatActivity {
             e.printStackTrace();
         }
 
-
+        String jsonGenre;
+        try {
+            jsonGenre = jsonObject.getString("genres");
+            jsonGenre = jsonGenre.replace("[","");
+            jsonGenre = jsonGenre.replace("]","");
+            jsonGenre = jsonGenre.replace("\"","");
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return;
+        }
         title.setText(strTitle);
         overview.setText(strOverview);
         tagLine.setText(strTagLine);
+        genre.setText(jsonGenre);
+        productedBy.setText("Producted by: ".concat(producers));
     }
+
+
 }
--- app/src/main/java/com/example/testetokenlab/MainActivity.java
+++ app/src/main/java/com/example/testetokenlab/MainActivity.java
@@ -228,7 +228,6 @@ public class MainActivity extends AppCompatActivity {
                         }
                     });
 
-
                 }
             });
 
